9b9f513dd59444f2eb314d9cb597275b4c3390db
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-15 21:35:45 +0200

    Added responsive layout

---
+++ src/components/Aside.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Aside() {
+  return <div className="aside">Aside</div>;
+}
+
+export default Aside;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NavBar() {
+  return <div className="navbar">NavBar</div>;
+}
+
+export default NavBar;
