64dfebd2e9639a2bb87ca8ca67d2bdb5ffc21e59
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-17 15:50:45 +0200

    Adding highlight section to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Aside from './components/Aside';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import NavBar from './components/NavBar';
+import Highlight from './components/Highlight';
 
 function App() {
   return (
     <div className="App">
+      <Highlight />
       <NavBar />
       <Aside />
       <Main />
---
+++ src/components/Highlight.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Highlight() {
+  return <div className="highlight">Highlight</div>;
+}
+
+export default Highlight;
