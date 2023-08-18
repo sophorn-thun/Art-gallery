4c2c1565880b399e36e6fe4f60607af59b032338
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-18 15:31:37 +0200

    Add welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Aside from './components/Aside';
+import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import NavBar from './components/NavBar';
-import Highlight from './components/Highlight';
 
 function App() {
   return (
     <div className="App">
-      <Highlight />
+      <Carousel />
       <NavBar />
       <Aside />
       <Main />
--- src/components/Highlight.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Highlight() {
-  return <div className="highlight">Highlight</div>;
-}
-
-export default Highlight;
