eb2d5981944e4b436c60027d742b664f32d26604
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-28 14:36:46 +0200

    Restyled home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import Button from '../components/Button/Button';
+// import Button from '../components/Button/Button';
 
 function Home() {
   return (
     <div>
       <div className="welcome-page">
         <div className="welcome-text">
-          <h2>React Art Gallery</h2>
-          <h3>Contemplating art from anywhere</h3>
-          <Button children="Gallery Entry" type="button" color="primary" />
+          <h1>Virtual Art Gallery</h1>
+          <h2>Contemplating art from anywhere</h2>
+          {/* <Button children="Gallery Entry" type="button" color="primary" /> */}
         </div>
       </div>
     </div>
