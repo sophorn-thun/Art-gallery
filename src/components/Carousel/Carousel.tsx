8443c0386579cc81a064b4fd8dcf8bfc516a0961
Sophorn Thun <thunsophorn012@gmail.com>
2023-09-01 10:53:16 +0200

    Adding carousel to home page

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,17 +1,49 @@
 import styles from './Carousel.module.css';
-import React from 'react';
+import { useState } from 'react';
 
-interface imagesProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-  images: string[];
-}
-function Carousel({ images, ...rest }: imagesProps) {
+function Carousel() {
+  const items = [
+    {
+      src: 'https://cdn.pixabay.com/photo/2017/12/28/16/18/bicycle-3045580_1280.jpg',
+      alt: 'Image 1 Carousel',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_1280.jpg',
+      alt: 'Image 2 Carousel',
+    },
+    {
+      src: 'https://cdn.pixabay.com/photo/2013/10/25/20/46/mosaic-200864_1280.jpg',
+      alt: 'Image 3 Carousel',
+    },
+  ];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % items.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + items.length) % items.length);
+  };
+
   return (
     <div className={styles['carousel']}>
-      <button className={styles['button-left']}>&lt;</button>
-      {images.map((image, index) => (
-        <img src={image} {...rest} key={index} />
-      ))}
-      <button className={styles['button-right']}>&gt;</button>
+      <button onClick={prevSlide} className={styles['button-left']}>
+        &lt;
+      </button>
+      <div className={styles['inner']}>
+        {items.map((item, index) => (
+          <div
+            className={styles['carousel-item']}
+            key={index}
+            style={{ display: currentSlide === index ? 'block' : 'none' }}
+          >
+            <img className={styles['carousel-img']} src={item.src} alt={item.alt} />
+          </div>
+        ))}
+      </div>
+      <button onClick={nextSlide} className={styles['button-right']}>
+        &gt;
+      </button>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-// import Button from '../components/Button/Button';
+import Carousel from '../components/Carousel/Carousel';
 
 function Home() {
   return (
@@ -7,9 +7,9 @@ function Home() {
         <div className="welcome-text">
           <h1>Virtual Art Gallery</h1>
           <h2>Contemplating art from anywhere</h2>
-          {/* <Button children="Gallery Entry" type="button" color="primary" /> */}
         </div>
       </div>
+      <Carousel />
     </div>
   );
 }
