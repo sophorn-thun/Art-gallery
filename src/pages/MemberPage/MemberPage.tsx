36021071f5b3fbdad109bbc34798ed999ba20466
Sophorn Thun <thunsophorn012@gmail.com>
2023-09-21 09:13:52 +0200

    Fixing save/unsave function

--- src/pages/MemberPage/MemberPage.tsx
+++ src/pages/MemberPage/MemberPage.tsx
@@ -5,6 +5,29 @@ import { GlobalStateContext } from '../../context/GlobalState';
 import ArtCard from '../../components/ArtCard/ArtCard';
 import { useArtworkById } from '../../hooks/useArtworkById';
 
+function SavedArtworkItem({ imageId }) {
+  const { artwork, isLoading, error } = useArtworkById(imageId.toString());
+
+  if (isLoading)
+    return (
+      <p>
+        <Loading />
+      </p>
+    );
+  if (error) return <p>Error loading artwork</p>;
+
+  return (
+    <ArtCard
+      key={artwork?.id}
+      id={artwork?.id}
+      title={artwork?.title}
+      artist_title={artwork?.artist_title}
+      image_id={artwork?.image_id}
+      iiifBaseUrl={artwork?.iiif_url}
+    />
+  );
+}
+
 function MemberPage() {
   const globalState = useContext(GlobalStateContext);
   if (!globalState) {
@@ -20,30 +43,9 @@ function MemberPage() {
         <>
           <h2 className={styles['welcome']}>Hello there! Here's your saved artwork collection:</h2>
           <div className={styles['saved-collection']}>
-            {savedImages.map((imageId) => {
-              const { artwork, isLoading, error } = useArtworkById(imageId.toString());
-
-              console.log('ImageId: ', imageId);
-
-              if (isLoading)
-                return (
-                  <p key={imageId}>
-                    <Loading />
-                  </p>
-                );
-              if (error) return <p key={imageId}>Error loading artwork</p>;
-
-              return (
-                <ArtCard
-                  key={artwork?.id}
-                  id={artwork?.id}
-                  title={artwork?.title}
-                  artist_title={artwork?.artist_title}
-                  image_id={artwork?.image_id}
-                  iiifBaseUrl={artwork?.iiif_url}
-                />
-              );
-            })}
+            {savedImages.map((imageId) => (
+              <SavedArtworkItem key={imageId} imageId={imageId} />
+            ))}
           </div>
         </>
       ) : (
