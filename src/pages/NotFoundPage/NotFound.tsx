64c1e678b7f12994b76167065d742dd795ecd587
Sophorn Thun <thunsophorn012@gmail.com>
2023-10-04 12:16:47 +0200

    Updating not found page

--- src/pages/NotFoundPage/NotFound.tsx
+++ src/pages/NotFoundPage/NotFound.tsx
@@ -1,7 +1,7 @@
 import styles from './NotFound.module.css';
 
 function NotFound() {
-  return <h1>Opp! The address you entered is not found!</h1>;
+  return <h1 className={styles['message']}>Opp! The address you entered does not exist!</h1>;
 }
 
 export default NotFound;
